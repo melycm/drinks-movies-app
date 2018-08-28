b37096ad759711e8d375e69a82fd6dbdf34b4161
Jeith <keithp1208@yahoo.com>
2018-08-28 10:32:07 -0500

    setting files up

---
+++ app.js
@@ -0,0 +1,20 @@
+let express = require('express');
+let app = express();
+var http = require('http').Server(app);
+var bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.locals.siteTitle = "Movies and Drinks";
+
+app.get("/", (req, res, next) => {
+    res.render('pages/index');
+});
+
+http.listen(7000, function () {
+    console.log('Listening on port 7000');
+});
