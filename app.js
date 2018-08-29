23e8763dffc94931694663630c926befc6e6ec5b
Namaslay33 <skylar.hunt@utexas.edu>
2018-08-29 15:09:16 -0500

    Added basic form pages

--- app.js
+++ app.js
@@ -8,12 +8,21 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(require('./form'));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", 
+    "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.locals.siteTitle = "Movies and Drinks";
 
 app.get("/", (req, res, next) => {
     res.render('pages/index');
 });
+
 
 http.listen(7000, function () {
     console.log('Listening on port 7000');
